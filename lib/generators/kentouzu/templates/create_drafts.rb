a09a294ff685c5f819b62020e2d1925fe0a826e0
Sean Eshbaugh <seaneshbaugh@gmail.com>
2012-10-30 14:03:59 -0500

    Fixed typo in install generator.

--- lib/generators/kentouzu/templates/create_drafts.rb
+++ lib/generators/kentouzu/templates/create_drafts.rb
@@ -1,6 +1,6 @@
 class CreateDrafts < ActiveRecord::Migration
   def change
-    create_table :drafts do |t|
+    create_table :kentouzu do |t|
       t.string :item_type,   :null => false
       t.integer :item_id,    :null => false
       t.string :event,       :null => false
